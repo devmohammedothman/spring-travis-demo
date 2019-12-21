6676eb2085523e0b751e85243684a51c5a033c55
devmohammedothman <dev.mohammed.othman@gmail.com>
2019-12-21 17:29:37 +0200

    first commit

---
+++ src/main/java/com/handonlab/springtravisdemo/SpringTravisDemoApplication.java
@@ -0,0 +1,13 @@
+package com.handonlab.springtravisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTravisDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTravisDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/handonlab/springtravisdemo/SpringTravisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handonlab.springtravisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTravisDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
